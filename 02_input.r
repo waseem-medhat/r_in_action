051ca68e2d31eedf5c523c92a43ac197d8a7a38c
waseem <waseemmedhatg@gmail.com>
2019-05-19 19:29:53 +0200

    finished ch02

---
+++ 02_input.r
@@ -0,0 +1,16 @@
+
+# Manual Input ------------------------------------------------------------
+
+mydata <- data.frame(
+  age = numeric(),
+  gender = character(),
+  weight = numeric()
+)
+
+mydata <- edit(mydata)
+fix(mydata) # alternative to the line above
+
+
+# Text Files --------------------------------------------------------------
+
+
