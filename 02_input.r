804aa6e901ffaf3ea5b7eeb007ab8ad6b7425598
waseem <waseemmedhatg@gmail.com>
2019-05-19 19:39:26 +0200

    changes to 02_input.r

--- 02_input.r
+++ 02_input.r
@@ -13,4 +13,19 @@ fix(mydata) # alternative to the line above
 
 # Text Files --------------------------------------------------------------
 
+read.table(
+  "data/grades.txt",
+  header = TRUE,
+  sep = ",",
+  row.names = 'StudentID',
+  stringsAsFactors = FALSE
+)
 
+# NOTE: 'incomplete final line' error results from the absence of newline
+#       character in the last line
+
+
+# SPSS --------------------------------------------------------------------
+
+Hmisc::spss.get("data/hep.sav")
+foreign::read.spss("data/hep.sav", to.data.frame = TRUE)
