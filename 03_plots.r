1958629aa7d876dc33b0c63918b1714fc73e14d3
waseem <waseemmedhatg@gmail.com>
2019-05-22 18:34:02 +0200

    added 03_plots.r up to colors

---
+++ 03_plots.r
@@ -0,0 +1,44 @@
+library(RColorBrewer)
+
+# Basic Plot and Output ---------------------------------------------------
+
+pdf('first_plot.pdf') # direct graphics to pdf instead of R output
+
+  plot(mtcars$wt, mtcars$mpg)
+  abline(lm(mpg ~ wt, data = mtcars))
+  title("Regression of MPG on Weight")
+
+dev.off() # turn off; graphics are directed to R output
+
+
+# Basic Example -----------------------------------------------------------
+
+dose <- c(20, 30, 40, 45, 60)
+drugA <- c(16, 20, 27, 40, 60)
+drugB <- c(15, 18, 25, 31, 40)
+
+plot(dose, drugA, type = 'b') # b for BOTH lines and points
+
+
+# Parameters --------------------------------------------------------------
+
+default <- par(no.readonly = TRUE) # save defaults
+
+par(lty = 2, pch = 17) # dashed line; triangular points
+plot(dose, drugA, type = 'b')
+
+par(default) # restore defaults
+
+# use parameters inside plot()
+plot(dose, drugA, type = 'b', 
+     lty = 3, pch = 18, cex = 2, lwd = 3)
+
+# lty = line type
+# lwd = line width
+# pch = point symbol
+# cex = point size
+
+# colors
+n <- 5
+thecolors <- brewer.pal(n, "Accent")
+pie(rep(1, n), labels = thecolors, col = thecolors)
