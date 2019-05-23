f85ca4fdcac1c494690443223b04f5ca51362e8c
waseem <waseemmedhatg@gmail.com>
2019-05-23 16:54:47 +0200

    03_plots.r added text/axes

--- 03_plots.r
+++ 03_plots.r
@@ -54,12 +54,83 @@ par(
   family    = "sans"
 )
 
-plot(dose, drugA, type = 'b',
-     main = "some title",
-     ) # b for BOTH lines and points
   
 # add font families
 windowsFonts(
   Hack = "Hack",
   ohno = "Comic Sans MS"
 )
+
+# margins
+par(pin=c(5,5), mai=c(1,.5, 1, .2))
+
+plot(dose, drugA, type = 'b',
+     main = "some title",
+     ) # b for BOTH lines and points
+
+par(default)
+
+
+# Text, Axes, and Legends -------------------------------------------------
+
+plot(
+  dose, drugA, type="b",
+  col="red", lty=2, pch=2, lwd=2,
+  # main="Clinical Trials for Drug A",
+  # sub="This is hypothetical data",
+  # xlab="Dosage",
+  # ylab="Drug Response",
+  xlim=c(0, 60),
+  ylim=c(0, 70),
+  ann = FALSE,
+  # axes = FALSE, # suppress all axes
+  frame.plot = TRUE, # if axes are suppressed keep the frame
+  xaxt = "n" # suppress x axis
+)
+
+title(
+  main = "Main Title",
+  col.main = "tomato",
+  sub  = "Subtitle",
+  xlab = "X label",
+  ylab = "Y label"
+)
+
+# example 3.2
+x <- 1:10
+y <- x
+z <- 10/x
+
+default <- par(no.readonly = TRUE) # save defaults
+
+par(mar = c(5, 4, 4, 8) + 0.1)
+
+plot(
+  x, y, type = 'b', pch  = 21, col  = 'red',
+  yaxt = 'n', lty  = 3, ann  = FALSE
+)
+
+lines(
+  x, z, type = 'b', pch  = 22,
+  col  = 'blue', lty  = 2
+)
+
+axis(
+  2, at = x, labels = x, col.axis = 'red', las = 2
+)
+
+axis(
+  4, at = z, labels = round(z, 2), col.axis = 'blue',
+  las = 2, cex.axis = 0.7, tck = -0.01
+)
+
+mtext(
+  "y = 1 / x", side = 4, line = 3,
+  cex.lab = 1, las = 2, col = 'blue'
+)
+
+title(
+  "An Example of Axes", xlab = "X", ylab = "Y = X"
+)
+
+par(default)
