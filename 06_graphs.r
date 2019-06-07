64aa8c623bdfb16f64c7289a1f444993b8539bf7
waseem <waseemmedhatg@gmail.com>
2019-06-08 01:49:23 +0200

    06_graphs.r pies, histograms, and kernels

--- 06_graphs.r
+++ 06_graphs.r
@@ -1,5 +1,7 @@
 library(vcd)
 library(gplots)
+library(plotrix)
+library(sm)
 
 # Bar Plots ---------------------------------------------------------------
 
@@ -62,4 +64,54 @@ spine(t(counts_2w))
 
 # Pie Charts --------------------------------------------------------------
 
+# data
+slices <- c(10, 12, 4, 16, 8)
+lbls <- c("US", "UK", "Australia", "Germany", "France")
 
+pcts <- round(slices / sum(slices) * 100)
+lbls2 <- paste0(lbls, ", ", pcts, "%")
+
+# pie
+pie(slices, lbls)
+pie(slices, lbls2)
+pie(table(state.region))
+
+# fan plot
+fan.plot(slices, labels = lbls2, main = "This is a fan plot.")
+
+
+# Histograms --------------------------------------------------------------
+
+# simple
+hist(mtcars$mpg)
+
+# density y-axis with rug and kernel density
+hist(mtcars$mpg, freq = FALSE, breaks = 12, col = "red")
+rug(jitter(mtcars$mpg, amount = 0.01))
+lines(density(mtcars$mpg), col = "blue", lwd = 2)
+
+# density y-axis with normal fit
+x <- mtcars$mpg
+xgaus <- seq(min(x), max(x), length = 50)
+ygaus <- dnorm(xgaus, mean(x), sd(x))
+
+hist(x, freq = FALSE, breaks = 12, col = "red")
+lines(xgaus, ygaus, lwd = 2)
+box()
+
+
+# Kernel Density Plots ----------------------------------------------------
+
+# density plot with colors and rug
+plot(density(x))
+polygon(density(x), col = "steelblue")
+rug(jitter(x, amount = 0.01))
+
+# compare densities
+cyl_f <- factor(
+  mtcars$cyl,
+  levels = c(4, 6, 8),
+  labels = c("4 cylinder", "6 cylinder", "8 cylinder")
+)
+
+sm.density.compare(mtcars$mpg, mtcars$cyl, lwd = 2)
