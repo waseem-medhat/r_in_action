404a2f459f20041d2c14cac352019434a69696b8
waseem <waseemmedhatg@gmail.com>
2019-06-23 17:09:30 +0200

    08_regression.r diagnostics

--- 08_regression.r
+++ 08_regression.r
@@ -1,3 +1,4 @@
+library(car)
 
 # Simple Linear Regression -------------------------------------------------------
 
@@ -59,3 +60,68 @@ fit_mult3 <- lm(
   data = mtcars
 )
 
+
+# Regression Diagnostics -------------------------------------------------------
+
+states <- as.data.frame(
+  state.x77[ ,c("Murder", "Population", "Illiteracy", "Income", "Frost")]
+)
+
+fit <- lm(Murder ~ ., data = states)
+confint(fit)
+
+# diagnostic plots
+par(mfrow = c(2,2))
+plot(fit)
+
+fit2 <- lm(weight ~ height + I(height ^ 2), data = women)
+plot(fit2)
+
+fit3 <- lm(weight ~ height + I(height ^ 2), data = women[-c(13, 15), ])
+plot(fit3)
+
+# `car` functions
+qqPlot(
+  fit,
+  id.method = "identify",
+  labels    = rownames(states),
+  simulate  = TRUE,
+  main      = "Q-Q Plot"
+)
+
+hist(rstudent(fit), breaks = 10)
+
+# residual plot
+residplot <- function(fit, nbreaks=10) {
+
+  z <- rstudent(fit)
+
+  hist(z, breaks=nbreaks, freq=FALSE,
+       xlab="Studentized Residual",
+       main="Distribution of Errors")
+
+  rug(jitter(z), col="brown")
+
+  curve(dnorm(x, mean=mean(z), sd=sd(z)),
+        add=TRUE, col="blue", lwd=2)
+
+  lines(density(z)$x, density(z)$y,
+        col="red", lwd=2, lty=2)
+
+  legend("topright",
+         legend = c("Normal", "Kernel"),
+         lty=1:2, col=c("blue","red"), cex=.7)
+}
+
+# durbin watson
+durbinWatsonTest(fit)
+
+# linearity: component+residual plot
+crPlots(fit)
+
+# homoscedasticity
+ncvTest(fit)
+spreadLevelPlot(fit)
+
+# muticollinearity
+vif(sqrt(fit))
