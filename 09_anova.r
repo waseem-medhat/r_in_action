51c2a75ad99310be8548a557923450475404c1f2
waseem <waseemmedhatg@gmail.com>
2019-07-01 13:27:06 +0200

    09_anova.r one way ancova

--- 09_anova.r
+++ 09_anova.r
@@ -1,6 +1,8 @@
 library(multcomp)
 library(gplots)
 library(car)
+library(effects)
+library(HH)
 
 ## NOTE: Anova() in the `car` library can perform type II or III SS
 ##       unlike base R's aov() which uses only type I
@@ -41,3 +43,39 @@ bartlett.test(response ~ trt, data = cholesterol)
 
 # outliers
 outlierTest(fit)
+
+
+# One-Way ANCOVA ----------------------------------------------------------
+
+head(litter)
+table(litter$dose)
+
+# summaries
+litter %>%
+  group_by(dose) %>%
+  summarize(
+    n    = n(),
+    mean = mean(weight),
+    sd   = sd(weight))
+
+# ancova fitting
+# dose controlled for gesttime
+fit <- aov(weight ~ gesttime + dose, data = litter)
+summary(fit)
+
+# adjusted group means
+effect(term = "dose", mod = fit)
+
+# multiple comparisons
+contrast <- rbind("no drug vs. drug" = c(3, -1, -1, -1))
+summary(glht(fit, linfct = mcp(dose = contrast)))
+
+# homogeneity of regression slopes
+summary(aov(weight ~ gesttime * dose, data = litter))
+
+# visualizing ancova
+ancova(weight ~ gesttime + dose, data = litter)
+ancova(weight ~ gesttime * dose, data = litter)
+
+
+# One-Way ANCOVA ----------------------------------------------------------
