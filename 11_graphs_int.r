755c20cc64059e2eaa07c97ec7aa21e2d8cc24d3
waseem <waseemmedhatg@gmail.com>
2019-07-13 01:29:26 +0200

    11_graphs_int.r line charts

--- 11_graphs_int.r
+++ 11_graphs_int.r
@@ -1,6 +1,7 @@
 library(car)
 library(hexbin)
 library(rgl)
+library(RColorBrewer)
 
 # Scatter Plots -----------------------------------------------------------
 
@@ -85,3 +86,55 @@ with(
 
 
 # Line Charts -------------------------------------------------------------
+
+# connecting the dots
+par_default <- par(no.readonly = TRUE)
+par(mfrow = c(1,2))
+
+t1 <- subset(Orange, Tree == 1)
+
+plot(t1$age, t1$circumference)
+plot(t1$age, t1$circumference, type = "b")
+
+par(par_default)
+
+# NOTE: plot() creates a new plot, while lines() adds a line to an
+#       existing plot
+
+# complex example
+Orange$Tree <- as.numeric(Orange$Tree)
+ntrees <- max(Orange$Tree)
+
+xrange <- range(Orange$age)
+yrange <- range(Orange$circumference)
+
+plot(
+  xrange, yrange,
+  type = "n",
+  xlab = "Age (days)",
+  ylab = "Circumference (mm)")
+
+colors <- brewer.pal(ntrees, "Set2")
+linetype <- c(1:ntrees)
+plotchar <- seq(18, 18 + ntrees, 1)
+
+for (i in 1:ntrees) {
+  tree <- subset(Orange, Tree == i)
+  lines(
+    tree$age, tree$circumference,
+    type = "b",
+    lwd = 2,
+    lty = linetype[i],
+    col = colors[i],
+    pch = plotchar[i])
+}
+
+title("Tree Growth")
+
+legend(
+  xrange[1], yrange[2],
+  1:ntrees,
+  col = colors,
+  pch = plotchar,
+  lty = linetype,
+  title = "Tree")
